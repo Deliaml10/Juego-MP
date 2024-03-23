8b589e2ff38ba0978333ad1ce78aada9da532790
Deliaml10 <114805861+Deliaml10@users.noreply.github.com>
2024-03-23 11:36:23 +0100

    Update Juego.java

--- Juego.java
+++ Juego.java
@@ -1,8 +1,7 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
-
 public class Juego {
-
     public static void main(String[] args) {
         // Mensaje de bienvenida
         try (Scanner scanner = new Scanner(System.in)) {
@@ -14,10 +13,10 @@ public class Juego {
             System.out.println("Presiona R para ver las reglas del juego o J para jugar:");
             String opcion = scanner.nextLine();
 
-            if (opcion.equalsIgnoreCase("R")) {
+            if (opcion.equalsIgnoreCase("R") || opcion.equalsIgnoreCase("r")) {
                 // Mostrar las reglas del juego
-                System.out.println("Aquí van las reglas del juego...");
-            } else if (opcion.equalsIgnoreCase("J")) {
+                System.out.println("El juego consiste en combates contra otros jugadores. Cada combate debe ser validado por un administrador.\nSi decides aceptar un desafío, podrás elegir tus armas y armaduras; el ganador se lleva el oro apostado.\nPor otro lado, si decides no aceptarlo, deberás pagar un 10% de lo que haya apostado el desafiante. ¡SUERTE! ");
+            } else if (opcion.equalsIgnoreCase("J") || (opcion.equalsIgnoreCase("j"))) {
                 // Opción de jugar
                 System.out.println("Seleccionaste jugar.");
 
@@ -25,21 +24,40 @@ public class Juego {
                 System.out.println("Presiona C para crear un nuevo usuario o I para iniciar sesión:");
                 String opcionJuego = scanner.nextLine();
 
-                if (opcionJuego.equalsIgnoreCase("C")) {
+                if (opcionJuego.equalsIgnoreCase("C") || opcionJuego.equalsIgnoreCase("c")) {
                     // Crear un nuevo usuario
                     System.out.println("Seleccionaste crear un nuevo usuario.");
-
+                    System.out.println("Presiona J para ser un jugador o A para ser un nuevo administrador");
+                    String eleccion = scanner.nextLine();
+                    if (eleccion.equalsIgnoreCase("A") || eleccion.equalsIgnoreCase("a")) {
+                        System.out.println("Seleccionaste ser un administrador.\n");
+                        //  Administrador admin = new Administrador();
+                    } else {
+                        System.out.println("Seleccionaste ser un nuevo jugador");
+                        //  Jugador jugador = new Jugador();  //ESTO FUNCIONARÁ CUANDO LA CLASE PERSONAJE ESTÉ CREADA
+                    }
                     // Pedir datos al usuario para crear el nuevo usuario
                     System.out.println("Ingresa tu nombre:");
                     String nombre = scanner.nextLine();
                     System.out.println("Ingresa tu nick:");
                     String nick = scanner.nextLine();
-                    System.out.println("Ingresa tu contraseña:");
-                    String contraseña = scanner.nextLine();
+                    String contrasena1;
+                    String contrasena;
+                    do {
+                        System.out.println("Ingresa tu contraseña:");
+                        contrasena = scanner.nextLine();
+                        System.out.println("Repite tu contreseña:");
+                        contrasena1 = scanner.nextLine();
+                        if (!contrasena1.equals(contrasena)) {
+                            System.out.println("Las contraseñas no coindicen");
+                        }
+                    }
+                    while (!contrasena1.equals(contrasena));
 
-                    // Aquí podrías realizar la lógica para crear el nuevo usuario con los datos proporcionados
+         //           Jugador jugador = new Jugador(String nombre, String contrasena, String nick);
                     System.out.println("¡Usuario creado exitosamente!");
-                } else if (opcionJuego.equalsIgnoreCase("I")) {
+
+                } else if (opcionJuego.equalsIgnoreCase("I") || opcionJuego.equalsIgnoreCase("i")) {      //OPCION DE INICIAR SESIÓN
                     // Iniciar sesión con un usuario existente
                     System.out.println("Seleccionaste iniciar sesión.");
 
@@ -47,7 +65,7 @@ public class Juego {
                     System.out.println("Ingresa tu nick:");
                     String nick = scanner.nextLine();
                     System.out.println("Ingresa tu contraseña:");
-                    String contraseña = scanner.nextLine();
+                    String contrasena = scanner.nextLine();
 
                     // Aquí podrías realizar la lógica para verificar el inicio de sesión con los datos proporcionados
                     System.out.println("¡Inicio de sesión exitoso!");
@@ -57,6 +75,7 @@ public class Juego {
             } else {
                 System.out.println("Opción no válida.");
             }
+
         }
     }
 }
