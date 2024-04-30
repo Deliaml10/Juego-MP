09c07e070a6171c10cfbdd3c0d767fa7a8a64624
Irene <i.garcialop.2021@alumnos.urjc.es>
2024-04-30 16:56:07 +0200

    test licntropo

--- out/Tests/TestTalento.java
+++ out/Tests/TestTalento.java
@@ -1,2 +1,15 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TestTalento {
+
+    @Test
+    public void testTalento() {
+        String nombre = "Destreza";
+
+        Talento talento = new Talento(nombre);
+
+        assertEquals(nombre, talento.getNombre());
+    }
 }
