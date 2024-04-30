adfdb0cb384b3e25c6cccaad2f626c3f149e47a7
Irene <i.garcialop.2021@alumnos.urjc.es>
2024-04-30 15:28:43 +0200

    testAddDesafioPendienteAdmin

--- out/Tests/TestJugador.java
+++ out/Tests/TestJugador.java
@@ -149,5 +149,21 @@ public class TestJugador {
 
         assertTrue(jugador2.getDesafiosPendientes().isEmpty());
     }
+    @Test
+    public void testAddDesafioPendienteAdmin() {
+        Jugador jugador1 = new Jugador("Nombre", "jugador1", "jugador1");
+        Jugador jugador2 = new Jugador("Nombre2", "jugador2", "jugador2");
+        Personaje personajeDesafiante = new Personaje("Desafiante", 1, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+        Personaje personajeDesafiado = new Personaje("Desafiado", 1, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+
+        jugador1.registrarPersonaje(personajeDesafiante);
+        jugador2.registrarPersonaje(personajeDesafiado);
+
+        jugador1.desafiar(jugador1, jugador2, 33);
+
+        Administrador admin = new Administrador("Nombre", "Contraseña", "admin");
+
+        assertEquals(1, admin.getDesafiosPendientesComunes().size());
+    }
 }
 
