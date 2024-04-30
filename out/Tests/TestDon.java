a853077fc28884f158204214529632850d1fd8bf
Deliaml10 <deliaml004@gmail.com>
2024-04-30 12:27:08 +0200

    don

--- out/Tests/TestDon.java
+++ out/Tests/TestDon.java
@@ -1,8 +1,13 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestDon {
     @Test
     public void TestDon(){
-
+        Don don = new Don("Cantar", 3, 3);
+        assertEquals("Cantar", don.getNombre());
+        assertEquals(3, don.getValorDon());
+        assertEquals(3, don.getRabia());
     }
 }
